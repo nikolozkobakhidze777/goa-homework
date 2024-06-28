07ef941ff7b2134652e30c3b282302860cf82388
nikolozkobakhidze777 <168841233+nikolozkobakhidze777@users.noreply.github.com>
2024-06-28 20:29:53 +0200

    Add files via upload

---
+++ day 7/classwork/classwork.py
@@ -0,0 +1,29 @@
+#1 დავალება
+
+name = "Anna"
+score = 96
+
+print(name)
+print(score)
+
+#2 დავალება
+#Storing the flight number
+flight_n = "BA0117"
+
+#storing the flight information
+destination = "New York"
+distance = 1580
+
+print(flight_n)
+print(destination)
+print(distance)
+
+#3 დავალება
+salary = 150000
+pay_raise = 10000
+
+#create a new variable names new_salary
+
+new_salary = salary + pay_raise
+print(new_salary)
+
---
+++ day 7/homework/homework.py
@@ -0,0 +1,41 @@
+#1 დავალება
+print(1121 + 1886)
+print(1367 - 1000)
+print(10789 * 128301)
+print(1200 // 2)
+print(70 ** 70)
+
+#2 დავალება
+moms_age = 45
+dads_age = 51
+
+#20 years later
+moms_age =int(input("დედის ასაკი: "))
+moms_age_in_20_years = moms_age + 20
+dads_age =int(input("მამის ასაკი: "))
+dads_age_in_20_years = dads_age + 20
+
+
+
+
+#3 დავალება
+name = "nikolozi"
+last_name = "kobakhidze"
+age = 11
+country = "georgia"
+favorite_color = "red"
+favorite_car = "bmw m3 competition"
+favorite_food = "mwvadi"
+favorite_sport = "football and rugby"
+
+print(name + "" + last_name + "" + country + "" + favorite_color + "" + favorite_car + "" + favorite_food + "" + favorite_sport)
+
+#4 დავალება
+name = input("გთხოვთ შეიყვანეთ თქვენი სახელი: ")
+print(f"გამარჯობა,{name}გილოცავ დაბადების დღეს")
+
+#5 დავალება
+cost = 15
+quantity = 30
+
+print(cost * quantity)
