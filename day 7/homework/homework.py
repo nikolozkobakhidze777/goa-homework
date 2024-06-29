b358cd7db6ad470ef39eca373042da6938c9ed42
nikolozkobakhidze777 <168841233+nikolozkobakhidze777@users.noreply.github.com>
2024-06-29 09:21:35 +0200

    Add files via upload

---
+++ day 7/homework/homework.py
@@ -0,0 +1,45 @@
+#1 დავალება
+number1 = int(input("enter number1: "))
+number2 = int(input("enter number2: "))
+number3 = int(input("enter number3: "))
+number4 = int(input("enter number4: "))
+
+print(number1 + number2 + number3 + number4)
+print(number1 - number2 - number3 - number4)
+print(number1 * number2 * number3 * number4)
+print(number1 // number2 // number3 // number4)
+
+#2 დავალება
+moms_age = 45
+dads_age = 51
+
+#20 years later
+moms_age =int(input("დედის ასაკი: "))
+moms_age_in_20_years = moms_age + 20
+dads_age =int(input("მამის ასაკი: "))
+dads_age_in_20_years = dads_age + 20
+
+
+
+
+#3 დავალება
+name = "nikolozi"
+last_name = "kobakhidze"
+age = 11
+country = "georgia"
+favorite_color = "red"
+favorite_car = "bmw m3 competition"
+favorite_food = "mwvadi"
+favorite_sport = "football and rugby"
+
+print(name + "" + last_name + "" + country + "" + favorite_color + "" + favorite_car + "" + favorite_food + "" + favorite_sport)
+
+#4 დავალება
+name = input("გთხოვთ შეიყვანეთ თქვენი სახელი: ")
+print(f"გამარჯობა,{name}გილოცავ დაბადების დღეს")
+
+#5 დავალება
+cost = 15
+quantity = 30
+
+print(cost * quantity)
