b5074960f1faeb8c42d4ee8457108ded1313fe51
nikolozkobakhidze777 <168841233+nikolozkobakhidze777@users.noreply.github.com>
2024-07-13 08:41:51 +0200

    Add files via upload

---
+++ day 9/homework/homework.py
@@ -0,0 +1,56 @@
+print(10 > 5)
+print(10 < 15)
+print(30 >= 32)
+print(23 <= 21)
+print(100 == 100)
+print(96 != 1823)
+print(10 + 5 == 5 + 10)
+print(20 > 15)
+print(30 < 40)
+print(40 >= 42)
+print(40 <= 40)
+print(124 == 124)
+print(76 != 123)
+print(20 + 5 == 5 + 20)
+print(34 > 12)
+print(433 < 1232)
+print(23 >= 32)
+print(123 <= 234)
+print(12332 == 12332)
+print(321 != 123)
+print(30 + 10 == 10 + 30)
+print(42 > 13)
+print(54 < 123)
+print(123 >= 123)
+print(323 <= 3123)
+print(12312 == 12312)
+print(345 != 123)
+print(320 + 10 == 10 + 320)
+print(323 > 133)
+print(1232 < 123343)
+
+
+first_person_age = 34
+second_person_age = 23
+
+first_person_age =int(input("enter first_person_age: "))
+second_person_age =int(input("enter second_person_age: "))
+
+print(34 > 23)
+
+str(input("enter who is older: "))
+
+
+savings = int(input("please enter your savings: "))
+if savings==150 :
+    print(" amount in 1 year :157,5")
+if savings== 250:
+    
+     print("amount in 1 year : 262,5")
+
+
+score=int(input("please enter your score: "))
+if score==120 :
+     print("true")
+if score==95 :
+     print("false")
