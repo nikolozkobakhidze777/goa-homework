005f0cc1c0fd0d4894ac2584b3ba31c9e84a6796
nikolozkobakhidze777 <168841233+nikolozkobakhidze777@users.noreply.github.com>
2025-07-11 18:11:49 +0400

    Add files via upload

---
+++ goa-homework/lvl 62/classwork/scriptt.js
@@ -0,0 +1,10 @@
+// საკლასო დავალება:
+
+// html-ში ჩაამატეთ ერთი სურათი.
+
+// js-ში წამოიღეთ ეს სურათი და უბრალოდ დაბეჭდეთ მისი src კუთვნილების მნიშვნელობა.
+
+const image = document.getElementById("img1");
+
+
+console.log("idk:", image.src);
---
+++ goa-homework/lvl 62/homework/script.js
@@ -0,0 +1,19 @@
+function changeText() {
+  document.getElementById("myParagraph").textContent = "The paragraph text has been changed.";
+}
+
+function changeColor() {
+  document.getElementById("colorBox").style.backgroundColor = "red";
+}
+
+function changeFontSize() {
+  document.getElementById("mainHeading").style.fontSize = "36px";
+}
+
+function hideDiv() {
+  document.getElementById("hideDiv").style.display = "none";
+}
+
+function showMessage() {
+  alert("This is your custom message!");
+}
